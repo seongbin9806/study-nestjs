bc494f96ee5aced9c070bfa5343068713ebea133
seongbinchoi <seongbin9806@naver.com>
2024-07-03 02:45:55 +0900

    init

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,138 @@
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+interface PostModel{
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 해린',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+];
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  // 1) GET /posts
+  //    모든 Post를 다 가져온다.
+  @Get()
+  getPosts(){
+    return posts;
+  }
+
+  // 2) GET /posts/:id
+  //    id에 해당되는 post를 가져온다.
+  //    예를 들어서 id=1일 경우 id가 1인 포스트를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string){
+    const post = posts.find((post) => post.id === +id);
+
+    if(!post){
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  // 3) POST /posts
+  //    POST를 생성한다.
+  @Post()
+  postPost(
+    @Body('author') author:string,
+    @Body('title') title:string,
+    @Body('content') content:string,
+  ){
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    }
+
+    posts = [
+      ...posts,
+      post,
+    ]
+
+    return post;
+  }
+
+  // 4) PUT /posts/:id
+  //    id에 해당되는 POST를 변경한다.
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ){
+    const post = posts.find(post => post.id === +id);
+
+    if(!post){
+      throw new NotFoundException();
+    }
+
+    if(author){
+      post.author = author;
+    }
+
+    if(title){
+      post.title = title;
+    }
+
+    if(content){
+      post.content = content;
+    }
+
+    posts = posts.map(prevPost => prevPost.id === +id? post : prevPost);
+
+    return post;
+  }
+
+  // 5) DELETE /posts/:id
+  //    id에 해당되는 POST를 삭제한다.
+  @Delete(':id')
+  deletePost(
+    @Param('id') id: string
+  ){
+    const post = posts.find(post => post.id === +id);
+    
+    if(!post){
+      throw new NotFoundException();
+    }
+
+    posts = posts.filter(post => post.id !== +id);
+
+    return id;
+  }
+}
